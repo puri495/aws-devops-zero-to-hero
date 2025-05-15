700ae9f5fdc88b5babbdd4eb89164426fdcf9df1
puri495 <166919465+puri495@users.noreply.github.com>
2025-05-16 00:18:09 +0530

    purimotor

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull puri495/simple-python-flask-app:latest
+docker pull puri495/puri-motors:v1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 puri495/simple-python-flask-app
+docker run -d -p 5000:5000 puri495/puri-motors:v1
