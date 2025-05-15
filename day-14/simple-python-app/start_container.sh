0d6ca22e3a33a34c4bdcc31114cbc8ad801d711e
puri495 <166919465+puri495@users.noreply.github.com>
2025-05-16 00:19:55 +0530

    docker

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull puri495/puri-motors:v1
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 puri495/puri-motors:v1
